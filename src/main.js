409c19d67c866a5d9912e55f153f14e08c96ba61
mariosh346@gmail.com <ma19914589>
2019-06-04 00:31:50 +0300

    Add routing

--- src/main.js
+++ src/main.js
@@ -3,12 +3,11 @@ import './plugins/vuetify'
 import App from './App.vue'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
-import VueRouter from 'vue-router'
-
-Vue.use(VueRouter)
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/router.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Search from './components/Search/search.vue'
+// import Projects from './views/Projects.vue'
+// import Team from './views/Team.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'search',
+      component: Search
+    }
+  ]
+})

121ff5b96cab865f66d2a72939a42670e4d508e0
mariosh346@gmail.com <ma19914589>
2019-06-04 21:55:39 +0300

    Get all the info for a movie

--- src/components/movieModal.vue
+++ src/components/movieModal.vue
@@ -1,10 +1,70 @@
 <template>
   <v-dialog v-model="item">
-    <VCard>    
-      <h1>{{ item.title }}</h1>
-      <v-btn @click="$router.go(-1)">
+    <VCard>
+      <!-- <v-btn @click="$router.go(-1)">
         close
-      </v-btn>
+      </v-btn> -->
+
+      <v-layout>
+        <v-flex xs8>
+          <v-card-title primary-title>
+            <div>
+              <div class="headline">
+                {{ item.title }}
+              </div>
+              <div>
+                {{ item.overview }}                
+              </div>
+              <div>
+                ({{ item.release_date }})             
+              </div>
+              <div
+                v-for="lang in item.spoken_languages"
+                :key="lang.name"
+              >
+                {{ lang.name }}
+              </div>
+              <div>
+                {{ item.poster_path }}
+              </div>
+            </div>
+          </v-card-title>
+        </v-flex>
+        <v-flex xs4>
+          <v-card-title primary-title>
+            <div>
+              <div>
+                <span class="headline">{{ item.vote_average }}</span>
+                /10
+              </div>
+              <div>
+                {{ item.vote_count }}
+              </div>
+              <div>
+                {{ item.budget }}
+              </div>              
+              <div>
+                {{ item.revenue }}
+              </div>
+            </div>
+          </v-card-title>
+        </v-flex>
+      </v-layout>
+      <v-divider light></v-divider>
+      <v-card-actions class="pa-3">
+        Rate this album
+        <v-spacer></v-spacer>
+        <v-text-field
+          placeholder="rate"
+          append-icon="star"
+          autofocus
+          clearable 
+          :rules="inputRules"
+          type="number"
+          max="10"
+        />
+        <!-- <v-icon>star_border</v-icon> -->
+      </v-card-actions>
     </VCard>
   </v-dialog>
 </template>
@@ -16,13 +76,19 @@ export default {
   data() {
     return {
       item: {},
-      id: this.$route.params.id
+      rate: null,
+      id: this.$route.params.id,
+      inputRules: [v => 10 >= v.length >= 0 || "Type from 0 to 10"]
     }
   },
   watch: {
     item (val) {
       if (!val)
         this.$router.go(-1)
+    },
+    rate (val) {
+      // if (val)
+        
     }
   },
   created() {
